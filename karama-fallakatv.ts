ea7f82aad9c77b47df0a008974327b37b3e1bfdf
elamri12xx <84471797+elamri12xx@users.noreply.github.com>
2022-05-28 18:07:31 +0100

    Zvh.ts

--- karama-fallakatv.ts
+++ karama-fallakatv.ts
@@ -1,8 +1,4 @@
 #EXTM3U
 #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=126288,FRAME-RATE=24,RESOLUTION=1720x1080
 https://elamri-stream-sport.tk/OneTimesport2.index.ts
-#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=126288,FRAME-RATE=24,RESOLUTION=852x720
-https://elamri-stream-sport.tk/OneTimesport2.index.ts
-#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=126288,FRAME-RATE=24,RESOLUTION=360x360
-https://elamri-stream-sport.tk/OneTimesport2.index.ts
 
