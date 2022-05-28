bcd1a91b00e8b2374e732875da8235b235a2b3d1
elamri12xx <84471797+elamri12xx@users.noreply.github.com>
2022-05-28 18:04:54 +0100

    Gju.ts

--- karama-fallakatv.ts
+++ karama-fallakatv.ts
@@ -1,7 +1,7 @@
 #EXTM3U
 #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=126288,FRAME-RATE=24,RESOLUTION=1720x1080
-https://reda-stream-fallaka.tk?id=12694
+https://elamri-stream-sport.tk/OneTimesport2.index.ts
 #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=126288,FRAME-RATE=24,RESOLUTION=852x720
-https://reda-stream-fallaka.tk?id=12694
+https://elamri-stream-sport.tk/OneTimesport2.index.ts
 #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=126288,FRAME-RATE=24,RESOLUTION=360x360
-https://reda-stream-fallaka.tk?id=12694
+https://elamri-stream-sport.tk/OneTimesport2.index.ts
